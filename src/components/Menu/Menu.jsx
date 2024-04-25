3ac39d4740ec9036c1fcdd1231be60ad9dd2efa5
jasnmar <jasnmar@gmail.com>
2024-04-25 11:19:52 -0400

    More experiment

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -9,11 +9,6 @@ function Menu({ children }) {
     setOpen((prevOpen) => !prevOpen);
   }
 
-      /** Discovery Challenge:
-     * In the MenuButton, MenuDropdown, and MenuItem components, 
-     * accept `toggle` and `open` props and just console log 
-     * both of them in all 3 components.
-     */
   return (
     <div className="menu">
       {React.Children.map(children, (child) => {
--- src/components/Menu/MenuDropdown.jsx
+++ src/components/Menu/MenuDropdown.jsx
@@ -1,9 +1,20 @@
 import "./Menu.css";
+import React from "react"
 
 function MenuDropdown({ children, isOpen, clickHandler }) {
   console.log('MenuDropdown - clickHandler: ', clickHandler)
   console.log('MenuDropdown - isOpen: ', isOpen)
-  return isOpen ? <div className="menu-dropdown">{children}</div> : null;
+
+  return isOpen ? 
+    <div className="menu-dropdown">{
+
+      React.Children.map(children, (child) => {
+        return React.cloneElement(child, {
+          clickHandler: clickHandler, isOpen: open
+        })
+      })
+      }
+    </div> : null;
 }
 
 export default MenuDropdown;
