338679624fdc9242e557643be7e3043d39125293
jasnmar <jasnmar@gmail.com>
2024-04-25 12:00:01 -0400

    Dropdown Context

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,16 +1,6 @@
 import "./Menu.css";
 import { useState, createContext } from "react";
 
-/**
- * Challenge:
- * Part 1:
- * 1. Create new context here in the Menu component. Make sure
- *    to export it from this file as well.
- * 2. Wrap the `div` below with the Context Provider
- * 3. Give the Provider a value of the boolean `false` (represents
- *    the hardcoded `open` state for now - we'll fix this very soon.)
- */
-
 const ThemeContext = createContext()
 
 function Menu({ children }) {
@@ -21,7 +11,7 @@ function Menu({ children }) {
   }
 
   return (
-    <ThemeContext.Provider value={false}>
+    <ThemeContext.Provider value={true}>
       <div className="menu">
         {children}
       </div>
--- src/components/Menu/MenuDropdown.jsx
+++ src/components/Menu/MenuDropdown.jsx
@@ -1,8 +1,19 @@
 import "./Menu.css";
+import { useContext } from "react";
+import { ThemeContext } from "./Menu";
 
-function MenuDropdown({ children, isOpen }) {
-  console.log("isOpen:", isOpen);
-  return isOpen ? <div className="menu-dropdown">{children}</div> : null;
+/**
+ * Challenge part 2:
+ * Pull in the value from context and update the conditional
+ * rendering code below to use that value instead of `open`,
+ * which used to be passed down via props.
+ */
+
+
+function MenuDropdown({ children }) {
+  const value = useContext(ThemeContext)
+  console.log("isOpen:", value);
+  return value ? <div className="menu-dropdown">{children}</div> : null;
 }
 
 export default MenuDropdown;
