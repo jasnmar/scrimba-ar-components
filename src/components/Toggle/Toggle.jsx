6a5f5600db908c4fd3a3457eca57ab48e0ab4d70
jasnmar <jasnmar@gmail.com>
2024-04-25 13:54:48 -0400

    Toggle Context

--- src/components/Toggle/Toggle.jsx
+++ src/components/Toggle/Toggle.jsx
@@ -1,13 +1,30 @@
-import { useState } from "react"
+import { useState, createContext } from "react";
 
-function Toggle() {
-    const [on, setOn] = useState(true)
-    function toggle() {
-        setOn(prevOn => !prevOn)
-    }
-    return (
-        <p>Toggle</p>
-    )
+const ToggleScheme = createContext();
+
+function Toggle({ children }) {
+  /**
+   * Challenge: set up context!
+   * Reminder of the steps:
+   * 1. Create a new context (outside the component, but in this file)
+   * 2. Export that context instance from the file so we
+   *    can use it eleswhere
+   * 3. Use the Context Provider to wrap the `children` returned
+   *    from this Toggle component
+   * 4. Pass the state values to the context value prop for access
+   *    from child components later on
+   */
+  const [on, setOn] = useState(true);
+  function toggle() {
+    setOn((prevOn) => !prevOn);
+  }
+  const values = {on, toggle}
+  return <>
+      <ToggleScheme.Provider value={values}>
+        {children}
+      </ToggleScheme.Provider>
+    </>;
 }
 
-export default Toggle
+export default Toggle;
+export { ToggleScheme };
