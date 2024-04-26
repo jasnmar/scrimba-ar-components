a0dbb3b6530bd36182d3e6e4121c98fcc0012987
jasnmar <jasnmar@gmail.com>
2024-04-26 11:53:06 -0400

    Custom UseEffect 1

---
+++ src/hooks/useEfffectOnUpdate.js
@@ -0,0 +1,14 @@
+function useEffectOnUpdate(callback, dependenciesArray) {
+
+}
+
+/**
+ * Mini-quiz:
+ * What are the 2 parameters in React.useEffect?
+ * 1. Callback function
+ * 2. Array of dependencies
+ * 
+ * Mini-challenge:
+ * Update our custom hook function to accept
+ * the same parameters we talked about above
+ */
