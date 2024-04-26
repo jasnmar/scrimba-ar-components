bb083386528bceb93de08837e5c7333bbac60ddd
jasnmar <jasnmar@gmail.com>
2024-04-26 11:15:08 -0400

    Challenge Start

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,16 @@ import "./App.css";
 import Menu from "./components/Menu/index"
 import Star from "./components/Star/Star";
 import Toggle from "./components/Toggle";
-
+/**
+ * Challenge: 
+ * Part 1: 
+ * Create a ToggleDisplay component and attach
+ * it to Toggle as Toggle.Display.
+ * 
+ * It should grab the `on` value from the Toggle context
+ * and render children by calling children as a function, 
+ * passing the `on` value to it. (E.g. children(on)).
+ */
 function App() {
   const sports = ["Tennis", "Recquetball", "Pickleball", "Squash"];
   function onChange() {
