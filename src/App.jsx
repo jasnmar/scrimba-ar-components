d73f4bea2b6993d1f334704f55e3ffe5373c464b
jasnmar <jasnmar@gmail.com>
2024-04-26 11:11:41 -0400

    Starting Point

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import "./App.css";
 import Menu from "./components/Menu/index"
 import Star from "./components/Star/Star";
+import Toggle from "./components/Toggle";
 
 function App() {
   const sports = ["Tennis", "Recquetball", "Pickleball", "Squash"];
@@ -15,20 +16,35 @@ function App() {
   }
   return (
     <>
-      <Star onChange={starChange} />
-
-      <br />
-      <Menu onOpen={onChange}>
-        <Menu.Button>Sports</Menu.Button>
-        <Menu.DropDown>
-          {sports.map((sport) => (
-            <Menu.Item key={sport}>{sport}</Menu.Item>
-          ))}
-        </Menu.DropDown>
-      </Menu>
+      <Toggle>
+        <Toggle.Button>
+          <Toggle.On>
+            <div className="box filled"></div>
+          </Toggle.On>
+          <Toggle.Off>
+            <div className="box"></div>
+          </Toggle.Off>
+        </Toggle.Button>
+      </Toggle>
     </>
   );
 }
+{/* <>
+<Star onChange={starChange} />
+
+<br />
+<Menu onOpen={onChange}>
+  <Menu.Button>Sports</Menu.Button>
+  <Menu.DropDown>
+    {sports.map((sport) => (
+      <Menu.Item key={sport}>{sport}</Menu.Item>
+    ))}
+  </Menu.DropDown>
+</Menu>
+</> */}
+
+
+
 
 export default App;
 
