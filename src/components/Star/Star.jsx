5aa7c11b281af9dd890ccb52c51422eb19ebdd15
jasnmar <jasnmar@gmail.com>
2024-04-26 15:08:33 -0400

    Badges

--- src/components/Star/Star.jsx
+++ src/components/Star/Star.jsx
@@ -2,17 +2,8 @@ import "./Star.css";
 import { BsStar, BsStarFill } from "react-icons/bs";
 import useToggle from "../../hooks/useToggle";
 
-
-/**
- * Challenge: Remove all parts of our Toggle component
- * (don't delete the react-icons stars though!) and use 
- * the useToggle hook to conditionally render the stars. 
- * Make sure to add an onClick that runs the `toggle` function
- * you get from useToggle()
- */
-
-function Star() {
-  const [on, toggle] = useToggle({})
+function Star({isOn=false}) {
+  const [on, toggle] = useToggle({initialValue: isOn})
 
   return (
     <div onClick={toggle}>
@@ -23,13 +14,3 @@ function Star() {
 }
 
 export default Star;
-{/* <Toggle onToggle={onChange}>
-<Toggle.Button>
-  <Toggle.On>
-    <BsStarFill className="star filled" />
-  </Toggle.On>
-  <Toggle.Off>
-    <BsStar className="star " />
-  </Toggle.Off>
-</Toggle.Button>
-</Toggle> */}
